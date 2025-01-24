ea6d95ea3473661bd877767e7fd3dc650ec40de4
mudasir232 <alimuddassire345@gmail.com>
2025-01-25 00:32:24 +0500

    🎉 Initial commit - CRUD Application

---
+++ crud/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace crud.Models
+{
+    public partial class Student
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? Gender { get; set; }
+        public int? Age { get; set; }
+        public string? FatherName { get; set; }
+    }
+}
---
+++ crud/Models/mydbContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace crud.Models
+{
+    public partial class mydbContext : DbContext
+    {
+        public mydbContext()
+        {
+        }
+
+        public mydbContext(DbContextOptions<mydbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseMySql("server=127.0.0.1;database=mydb;user id=root;password=mudasir", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.35-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8mb4_0900_ai_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.ToTable("student");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Age).HasColumnName("age");
+
+                entity.Property(e => e.FatherName)
+                    .HasMaxLength(50)
+                    .HasColumnName("father_name");
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(50)
+                    .HasColumnName("first_name");
+
+                entity.Property(e => e.Gender)
+                    .HasMaxLength(10)
+                    .HasColumnName("gender");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
